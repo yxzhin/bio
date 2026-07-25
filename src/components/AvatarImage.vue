6be682f5fb2c5c1e5ad6c376b3a9c990fca1fd12
yxzhin <yxzhin@gmail.com>
2026-07-25 18:04:10 +0200

    implement AvatarImage

---
+++ src/components/AvatarImage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img
+    class="w-24 h-24 rounded-full mx-auto block object-cover"
+    alt="avatar"
+    src="./../assets/yxzhin.png"
+  />
+</template>
