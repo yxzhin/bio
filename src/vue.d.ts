201612c0e6b08513fb2d068dd555cd1d1a5a202c
yxzhin <yxzhin@gmail.com>
2026-07-25 19:12:35 +0200

    fix prod build bugs

---
+++ src/vue.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
+
+  const component: DefineComponent<{}, {}, any>
+
+  export default component
+}
